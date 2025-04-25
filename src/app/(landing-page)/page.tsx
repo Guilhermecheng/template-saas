9a2e0ca91364c3754ccaf518284f42b80705e329
Guilhermecheng <guilherme.cheng@gmail.com>
2025-04-25 10:35:13 -0300

    style: buttons and link

--- src/app/(landing-page)/page.tsx
+++ src/app/(landing-page)/page.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export default function LandingPage() {
   return (
-    <div className="flex items-center justify-center h-screen w-screen">
+    <div className="flex flex-col gap-8 items-center justify-center h-screen w-screen">
       <h1 className="text-2xl font-bold">Landing Page</h1>
 
-      <Link href="/login" className="border rounded-md px-1">Ir para login</Link>
+      <Link href="/login" className="border rounded-xl px-4">Ir para login</Link>
     </div>
   );
 }
